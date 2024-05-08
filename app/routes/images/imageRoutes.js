de1b42d68ddfa48e44e39a54399b0574ac464e0e
mtechub-pvt-ltd <mtechub.info@gmail.com>
2024-05-09 02:38:21 +0500

    new test

--- app/routes/images/imageRoutes.js
+++ app/routes/images/imageRoutes.js
@@ -7,8 +7,11 @@ const multer = require('multer');
 
 const upload = multer({ dest: 'uploads/' });
 
-router.post('/v1/uploadimage', upload.single('image'), imageController.uploadimage);
-router.delete('/v1/deleteimage', imageController.deleteimage);
-router.get('/v1/getAllimages', imageController.getAllimages);
-
+router.post('/v1/uploadimage',
+    upload.single('image'), imageController.uploadimage);
+router.delete('/v1/deleteimage',
+    imageController.deleteimage);
+router.get('/v1/getAllimages',
+    imageController.getAllimages);
+// lsasalh k but this is the correct one
 module.exports = router;
